fbdde94e9c31662ae9ee06fe039c0728e178947d
Sapnil-Bhowmick <sapnil.bhowmik@springandriver.com>
2024-04-11 10:10:07 +0530

    Changes made

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -47,7 +47,7 @@ const Footer = () => {
           ))}
         </div>
 
-        <div className="flex justify-between text-white-400 mt-24 max-sm:flex-col max-sm:items-center w-full">
+        <div className="flex justify-between items-center text-white-400 mt-24 max-sm:flex-col max-sm:items-center w-full">
           <div className="flex flex-1 justify-start items-center gap-2 font-montserrat cursor-pointer">
             <img
               src={copyrightSign}
